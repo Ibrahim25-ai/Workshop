5098217b112ca86bc13a775192d773853a87c75d
unknown <benlakhal19@gmail.com>
2023-04-09 11:05:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBare from "./Components/NavBare";
+import { Array } from "./Components/Array";
+import Forme from "./Components/Forme";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBare />
+      <h1>Helloooo</h1>
+      <Array />
+      <Forme />
+      <Forme />
+      <Forme />
+      <Forme />
+      <Forme />
+      <Forme />
+      <Forme />
     </div>
   );
 }
---
+++ src/Components/Array.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Table } from 'react-bootstrap'
+import Forme from './Forme'
+
+export const Array = () => {
+  return (
+    <div>
+        <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <Forme/>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  )
+}
